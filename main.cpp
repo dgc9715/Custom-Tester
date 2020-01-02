662997bfd12c9910f2a24cbbe13b192039c48773
dgc9715 <dgc9715@gmail.com>
2020-01-01 21:24:43 -0500

    start on the center of screen

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.move(QApplication::desktop()->screen()->rect().center() - w.rect().center());
     w.show();
 
     return a.exec();
